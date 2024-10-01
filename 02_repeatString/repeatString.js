83ba8ad3d3651e647082c0b99c0383eab75275f4
friedkaiju <61223378+friedkaiju@users.noreply.github.com>
2024-10-01 16:16:46 +1000

    done exercises 3->4

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,19 @@
-const repeatString = function() {
 
+const repeatString = (word, count) => {
+    let string = ""
+    if (count < 0)
+        return "ERROR"
+    for (let i = 0; i < count; i++) {
+        console.log(word, count)
+        string += word;
+    }
+    return string;
 };
+
+//console.log(repeatString(word.toString(), parseInt(count)))
+
+// repeatString('hey', 3) //return string a number of times
+//if statement does not need {to be wrapped}
 
 // Do not edit below this line
 module.exports = repeatString;
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = (str) => {
+    let splitString = str.split("");
+    console.log(splitString)
+    let reverseArray = splitString.reverse();
+    console.log(reverseArray);
+    let joinArray = reverseArray.join("");
+    return joinArray
 };
 
 // Do not edit below this line
